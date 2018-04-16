3ec9b930bd61ead49779227aafdee54c86e83702
copenbacon <sclary50@gmail.com>
2018-04-16 04:03:32 -0700

    first steps for countdown app

---
+++ countdown_champ/src/app.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			deadline: 'December 25, 2017'
+		}
+	}
+
+	change_deadline() {
+		this.setState({deadline: "November 25, 2017"})
+	}
+
+	render() {
+		return(
+			<div className="App">
+				<div className="App-title">Countdown to {this.state.deadline}</div>
+				<div>
+					<div className="Clock-days">Days</div>
+					<div className="Clock-hours">Hours</div>
+					<div className="Clock-minutes">Minutes</div>
+					<div className="Clock-seconds">Seconds</div>
+				</div>
+				<div>
+					<input placeholder='new date'/>
+					<button onClick={() => this.change_deadline()}>
+					Submit
+					</button>
+				</div>
+			</div>
+			)
+	}
+}
+
+export default App;
